62006e99e2d45d23821e3a39557957175436e549
viniciusdnb <viniciusdnb@hotmail.com>
2025-12-04 15:37:20 -0300

    funcao publica comum para verificar erro do validator e criar mensagem

---
+++ app/libs/verifyErrorValidator.js
@@ -0,0 +1,23 @@
+const { validationResult } = require('express-validator');
+const msgNotify = require('./msgNotify');
+//funcao que padrao para verificar se ha erros na validação e gera mensagem na sessao e retorna true se contiver erro
+module.exports = {
+    getResult: function (req) {
+        let errorResult = validationResult(req);
+        if (!errorResult.isEmpty()) {
+            let arrMsgError = errorResult.array();
+            let msg = "";
+            arrMsgError.forEach(msgError => {
+                msg += msgError.msg + " - ";
+            });
+
+            if (!req.session.msg) {
+                req.session.msg = msgNotify.getMsgWarning("Atenção", msg)
+            }
+
+           return true;
+        }
+
+        return false;
+    }
+}
